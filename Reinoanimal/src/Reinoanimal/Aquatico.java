c4829683928ac2d47fa8438fecd8ef0f60e87208
Botijaodegas <20191064010038@CANG278280.ifrn.local>
2023-11-20 15:52:01 -0300

    Add classe aquario

---
+++ Reinoanimal/src/Reinoanimal/Aquatico.java
@@ -0,0 +1,42 @@
+ package Reinoanimal;
+
+import java.time.LocalDate;
+
+public class Aquatico extends Animal {
+    private String tipoAgua;
+    private boolean possuiEscamas;
+
+    public Aquatico(String especie, char sexo, LocalDate dataNascimento, String tipoAgua, boolean possuiEscamas) {
+        super(especie, sexo, dataNascimento);
+        incrementarContador();
+        this.tipoAgua = tipoAgua;
+        this.possuiEscamas = possuiEscamas;
+    }
+
+    public void comer() {
+        System.out.println("O animal aquático está comendo...");
+    }
+
+    
+    public void perfil() {
+        super.perfil(); 
+        System.out.println("Tipo de Água: " + tipoAgua);
+        System.out.println("Possui Escamas: " + possuiEscamas);
+    }
+
+public String getTipoAgua() {
+ return tipoAgua;
+}
+
+public void setTipoAgua(String tipoAgua) {
+ this.tipoAgua = tipoAgua;
+}
+
+public boolean isPossuiEscamas() {
+ return possuiEscamas;
+}
+
+public void setPossuiEscamas(boolean possuiEscamas) {
+ this.possuiEscamas = possuiEscamas;
+}
+}
